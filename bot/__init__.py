c9472abff6779f3f884c47445da8a546294cb321
ixelizm <felekburak8@gmail.com>
2022-06-01 20:37:28 +0300

    bot hazırlık

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,23 @@
+from pyrogram import Client
+from requests import get
+from config import *
 
+ixel = Client(
+  name = "ixel",
+  api_id = API_ID,
+  api_hash = API_HASH,
+  session_string = SESSION_STRING,
+  workers = 24,
+  sleep_threshold = 60
+)
+
+url = "https://ixelizmplugins.herokuapp.com/api"
+r = get(url)
+data = r.json()
+
+
+for file, content in data.items():
+  with open(f"ixelator/modules/{file}.py", "w") as f:
+    f.write(content)
+  print(file, "Plugini Yüklendi!")
+ixel.plugins = dict(root="ixelator/modules")
